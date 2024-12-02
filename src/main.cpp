87904a53f5ac23cb06c56e6c5c3020f63aa57316
Michał Charaszkiewicz <michal.charaszkiewicz@gmail.com>
2024-12-02 16:20:56 +0100

    main file for testing Flac functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
+#include "Flac.hpp"
 #include <iostream>
+#include <stdexcept>
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    try
+    {
+        Flac flac_file("../audio/sample3.flac");
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "Error: " << e.what() << '\n';
+        return 1;
+    }
     return 0;
 }
