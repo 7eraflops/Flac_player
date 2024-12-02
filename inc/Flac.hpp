7d52ab6bfaebe7bd7d319197d7cd8c0626ebd9fc
Michał Charaszkiewicz <michal.charaszkiewicz@gmail.com>
2024-12-02 16:26:29 +0100

    iedntify the "fLaC" marker #1

--- inc/Flac.hpp
+++ inc/Flac.hpp
@@ -13,6 +13,7 @@ private:
 public:
     explicit Flac(const std::string &filename);
     ~Flac();
+    void check_flac_marker();
 };
 
 Flac::Flac(const std::string &filename)
@@ -29,5 +30,17 @@ Flac::~Flac()
     if (flac_file.is_open())
     {
         flac_file.close();
+    }
+}
+
+void Flac::check_flac_marker()
+{
+    // The "fLaC" marker is expected in the first 4 bytes of the file.
+    constexpr std::array<char, 4> flac_marker = {'f', 'L', 'a', 'C'};
+    std::array<char, 4> marker;
+    flac_file.read(marker.data(), marker.size());
+    if (!flac_file || !std::equal(marker.begin(), marker.end(), flac_marker.begin()))
+    {
+        throw std::runtime_error("File is not a valid FLAC file");
     }
 }
