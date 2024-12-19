89d77f25b934ef4c51ad507b262493f13ec05dce
Michał Charaszkiewicz <michal.charaszkiewicz@gmail.com>
2024-12-19 16:23:22 +0100

    testing of fixes. 16 bit decoding works

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <cstdint>
 #include <cstring>
 #include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <stdexcept>
 #include <vector>
@@ -11,17 +12,10 @@
 class WavFileWriter
 {
 public:
-    enum class BitDepth
-    {
-        Bit16 = 16,
-        Bit24 = 24,
-        Bit32 = 32
-    };
-
     WavFileWriter(const std::string &filename,
                   uint32_t sampleRate,
                   uint16_t numChannels,
-                  BitDepth bitDepth)
+                  uint8_t bitDepth)
         : m_filename(filename),
           m_sampleRate(sampleRate),
           m_numChannels(numChannels),
@@ -44,13 +38,13 @@ public:
         // Assumes buffer is already interleaved
         switch (m_bitDepth)
         {
-        case BitDepth::Bit16:
+        case 16:
             writeInterleavedSamples<int16_t>(reinterpret_cast<const int16_t *>(buffer), numSamples);
             break;
-        case BitDepth::Bit24:
+        case 24:
             writeInterleavedSamples<int32_t>(reinterpret_cast<const int32_t *>(buffer), numSamples);
             break;
-        case BitDepth::Bit32:
+        case 32:
             writeInterleavedSamples<int32_t>(reinterpret_cast<const int32_t *>(buffer), numSamples);
             break;
         default:
@@ -149,7 +143,7 @@ private:
     std::string m_filename;
     uint32_t m_sampleRate;
     uint16_t m_numChannels;
-    BitDepth m_bitDepth;
+    uint8_t m_bitDepth;
     uint16_t m_bytesPerSample;
     size_t m_totalSamplesWritten;
 };
@@ -167,10 +161,9 @@ std::vector<int16_t> convert_to_int16(const std::vector<int32_t> &int32_vec)
     return int16_vec;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-    std::string filename = "../audio/input/block_size_4096.flac";
-    WavFileWriter wavWriter("../audio/output/output.wav", 44100, 1, WavFileWriter::BitDepth::Bit16);
+    std::string filename = argv[1];
     std::ifstream flac_stream;
     try
     {
@@ -189,17 +182,14 @@ int main()
 
         int sample_rate = player.get_stream_info().sample_rate;
         int channels = player.get_stream_info().channels;
-        uint32_t frame_index{};
+        int bit_depth = player.get_stream_info().bits_per_sample;
+        WavFileWriter wavWriter("../audio/output/output.wav", sample_rate, channels, bit_depth);
         while (!player.get_reader().eos()) // Replace with actual condition
         {
-            auto start = std::chrono::high_resolution_clock::now();
             player.decode_frame();
             const std::vector<buffer_sample_type> &buffer = player.get_audio_buffer();
             size_t buffer_size = buffer.size();
             std::vector<int16_t> newbuf = convert_to_int16(buffer);
-            auto end = std::chrono::high_resolution_clock::now();
-            auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-            std::cout << "Function execution time: " << duration.count() << " milliseconds.\t" << "Frame: " << frame_index++ << std::endl;
 
             wavWriter.writeSamples(newbuf.data(), buffer_size);
         }
